f23884ce821d12c9a584286237b5b97c2d0722f7
ronyaakov19496 <ronyaakov19496@gmail.com>
2023-05-29 16:27:41 +0300

    ron

--- try1/calculation/calculation.ts
+++ try1/calculation/calculation.ts
@@ -1,13 +1,19 @@
 //---model---//
-class sendingDataCalculationsClass {
+class SendingDataCalculationsClass {
   uid: string;
-  constructor(public moneyNeed: string, public manyMonths: string) {
+  constructor(
+    public moneyNeed: number, 
+    public manyMonths: number,
+    public total:number,
+    public amla:number,
+    public returnMoney:number){
     this.uid = uid();
   }
 }
 
-let saveDateArray: sendingDataCalculationsClass[] = [];
+let saveDateArray: SendingDataCalculationsClass[] = [];
 //---model---//
+
 
 //---controller---//
 function getDateForm(evt) {
@@ -15,46 +21,80 @@ function getDateForm(evt) {
     evt.preventDefault();
     let moneyNeed = evt.target.elements.moneyNeed.value;
     let manyMonths = evt.target.elements.manyMonths.value;
-    saveDateArray.push(new sendingDataCalculationsClass(moneyNeed, manyMonths));
-    // console.log(saveDateArray)
-    // let monthsToYears = manyMonths * 12;
+    // let total = evt.target.elements.manyMonths.value;
+    let manyMONthsMultiplay=manyMonths*12
+    let total=moneyNeed/manyMONthsMultiplay
 
-    if (moneyNeed <= 5000) {
+    // console.log(`this is your total ${total}`)
+    
+    
+    let amla: number = 0
+    let returnMoney: number = 0
+
+    if (moneyNeed <= 5000) {      // monthsToYears
       console.log("5k");
+      amla +=total //amla = amla + totals
+      //returnMoney.innerHTML = "<h3>You need: 5k</h3>"
+
     } else if (moneyNeed <= 10000) {
       console.log("10k");
+      amla = 5
+      //returnMoney.innerHTML = "<h3>You need: 10K</h3>"
     } else {
       console.log("baziga!");
+      amla = 100
+      //returnMoney.innerHTML = "<h3>You need: 100k</h3>"
     }
 
     if (manyMonths <= 5) {
       console.log("time jukjsa");
+      returnMoney = moneyNeed/5
     } else if (manyMonths <= 10) {
+      returnMoney = moneyNeed/10
       console.log("76");
     } else {
       console.log("name is 23123 ");
+      returnMoney = moneyNeed/15
     }
-    renderDate.innerHTML = renderToItems(saveDateArray);
+   
+    saveDateArray = getLocalStorage()
+    saveDateArray.push(new SendingDataCalculationsClass(moneyNeed,manyMonths,total, amla, returnMoney));
+    // console.log(saveDateArray)
+    // let monthsToYears = manyMonths * 12;
+    // console.log(`tis ${monthsToYears}`)
+    saveToLocalStorage(saveDateArray)
+    // getLocalStorage(saveDateArray)  
+    // getLocalStorage()
+    // renderToScrrim()
+    renderToItems(saveDateArray)
+   // renderDate.innerHTML = renderToItems(saveDateArray);
   } catch (error) {
     console.log(error);
   }
 }
 
-function renderToItems(saveDateArray: sendingDataCalculationsClass[]): string {
+function renderToItems(saveDateArray: SendingDataCalculationsClass[]): string {
   try {
     if (!saveDateArray || !Array.isArray(saveDateArray))
       throw new Error("this items is not good");
 
+      //if(saveDateArray.lenght == 0) {
+      //  return `No data here`
+      //}
     let html = saveDateArray
       .map((userCalculations) => {
         return `<div>
-            <div>${userCalculations.moneyNeed}</div>
-            <div>${userCalculations.manyMonths}</div>
+            <div>Amount need: ${userCalculations.moneyNeed}</div>
+            <div>Amount of Months${userCalculations.manyMonths}</div>
+            <div>Total:${userCalculations.total}</div>
+            <div>Amla: ${userCalculations.amla}</div>
+            <div>Return Money: ${userCalculations.returnMoney}</div>
             <div><button onclick="deleteItemscalculations('${userCalculations.uid}')">remove</button></div>
             </div>
             `;
       })
       .join(" ");
+      renderDate.innerHTML = html
     // console.log(html)
     return html;
   } catch (error) {
@@ -62,16 +102,26 @@ function renderToItems(saveDateArray: sendingDataCalculationsClass[]): string {
     return `this `;
   }
 }
+function renderToScrrim(){
+  // saveToLocalStorage(saveDateArray)
+  //  getLocalStorage()
+  // renderToItems(saveDateArray)
+  const data = getLocalStorage() 
+  renderDate.innerHTML = renderToItems(data);
+  
+}
 
 function deleteItemscalculations(uid: string) {
   try {
     let index = saveDateArray.findIndex((items) => items.uid === uid);
     if (index === -1) throw new Error("we are have Error");
     saveDateArray.splice(index, 1);
-    renderDate.innerHTML = renderToItems(saveDateArray);
+    // add save to local storage function
+    renderToScrrim() 
   } catch (error) {
     console.log(error);
   }
+  renderToItems(saveDateArray)
 }
 //---controller---//
 
@@ -85,3 +135,61 @@ function uid(): string {
   return `id-${Math.random()}`;
 }
 //--helper--//
+// let h1=Number(prompt("years"))
+  
+//   let test=h1*12
+//   console.log(test)
+  // let action=prompt("choos")
+// function test(h1:number,h2:number,action:string|null){
+  
+//   if(action==="*"){
+//     console.log(h1*h2)
+//   }else if(action==="/"){
+//     console.log(h1/h2)
+//   }else{
+//     console.log("this is no ")
+//   }
+// }
+// test(h1,h2,action)
+
+function saveToLocalStorage(saveDateArray:SendingDataCalculationsClass[]){
+try {
+  // console.log(saveDateArray)
+  // if(!saveDateArray){
+    localStorage.setItem("items",JSON.stringify(saveDateArray));
+  // }else{
+    // console.log("")
+    // throw new Error("thihs is not good");
+  // }
+} catch (error) {
+  console.log(error);
+}
+}
+
+function getLocalStorage():saveDateArray[] {
+  // console.log(saveDateArray)
+  let data : string | null = localStorage.getItem("items")
+  console.log("datasssss",data)
+
+  if(data == null) {
+    return []
+  }
+
+  let parse=JSON.parse(data)
+  console.log("JSON.parse",parse)
+
+
+  return parse;
+
+  // console.log(parse)
+  // console.log("rrrrrrrrrR",d     ate)
+}
+
+//getLocalStorage()
+
+
+// const _items=getLocalStorage()
+// if(_items){
+//   saveDateArray.push(..._items)
+// }
+// renderDate(saveDateArray)
--- try1/calculation/dist/calculation.js
+++ try1/calculation/dist/calculation.js
@@ -1,11 +1,14 @@
 //---model---//
-var sendingDataCalculationsClass = /** @class */ (function () {
-    function sendingDataCalculationsClass(moneyNeed, manyMonths) {
+var SendingDataCalculationsClass = /** @class */ (function () {
+    function SendingDataCalculationsClass(moneyNeed, manyMonths, total, amla, returnMoney) {
         this.moneyNeed = moneyNeed;
         this.manyMonths = manyMonths;
+        this.total = total;
+        this.amla = amla;
+        this.returnMoney = returnMoney;
         this.uid = uid();
     }
-    return sendingDataCalculationsClass;
+    return SendingDataCalculationsClass;
 }());
 var saveDateArray = [];
 //---model---//
@@ -15,28 +18,50 @@ function getDateForm(evt) {
         evt.preventDefault();
         var moneyNeed = evt.target.elements.moneyNeed.value;
         var manyMonths = evt.target.elements.manyMonths.value;
-        saveDateArray.push(new sendingDataCalculationsClass(moneyNeed, manyMonths));
-        // console.log(saveDateArray)
-        // let monthsToYears = manyMonths * 12;
-        if (moneyNeed <= 5000) {
+        // let total = evt.target.elements.manyMonths.value;
+        var manyMONthsMultiplay = manyMonths * 12;
+        var total = moneyNeed / manyMONthsMultiplay;
+        // console.log(`this is your total ${total}`)
+        var amla = 0;
+        var returnMoney = 0;
+        if (moneyNeed <= 5000) { // monthsToYears
             console.log("5k");
+            amla += total; //amla = amla + totals
+            //returnMoney.innerHTML = "<h3>You need: 5k</h3>"
         }
         else if (moneyNeed <= 10000) {
             console.log("10k");
+            amla = 5;
+            //returnMoney.innerHTML = "<h3>You need: 10K</h3>"
         }
         else {
             console.log("baziga!");
+            amla = 100;
+            //returnMoney.innerHTML = "<h3>You need: 100k</h3>"
         }
         if (manyMonths <= 5) {
             console.log("time jukjsa");
+            returnMoney = moneyNeed / 5;
         }
         else if (manyMonths <= 10) {
+            returnMoney = moneyNeed / 10;
             console.log("76");
         }
         else {
             console.log("name is 23123 ");
+            returnMoney = moneyNeed / 15;
         }
-        renderDate.innerHTML = renderToItems(saveDateArray);
+        saveDateArray = getLocalStorage();
+        saveDateArray.push(new SendingDataCalculationsClass(moneyNeed, manyMonths, total, amla, returnMoney));
+        // console.log(saveDateArray)
+        // let monthsToYears = manyMonths * 12;
+        // console.log(`tis ${monthsToYears}`)
+        saveToLocalStorage(saveDateArray);
+        // getLocalStorage(saveDateArray)  
+        // getLocalStorage()
+        // renderToScrrim()
+        renderToItems(saveDateArray);
+        // renderDate.innerHTML = renderToItems(saveDateArray);
     }
     catch (error) {
         console.log(error);
@@ -46,11 +71,15 @@ function renderToItems(saveDateArray) {
     try {
         if (!saveDateArray || !Array.isArray(saveDateArray))
             throw new Error("this items is not good");
+        //if(saveDateArray.lenght == 0) {
+        //  return `No data here`
+        //}
         var html = saveDateArray
             .map(function (userCalculations) {
-            return "<div>\n            <div>" + userCalculations.moneyNeed + "</div>\n            <div>" + userCalculations.manyMonths + "</div>\n            <div><button onclick=\"deleteItemscalculations('" + userCalculations.uid + "')\">remove</button></div>\n            </div>\n            ";
+            return "<div>\n            <div>Amount need: " + userCalculations.moneyNeed + "</div>\n            <div>Amount of Months" + userCalculations.manyMonths + "</div>\n            <div>Total:" + userCalculations.total + "</div>\n            <div>Amla: " + userCalculations.amla + "</div>\n            <div>Return Money: " + userCalculations.returnMoney + "</div>\n            <div><button onclick=\"deleteItemscalculations('" + userCalculations.uid + "')\">remove</button></div>\n            </div>\n            ";
         })
             .join(" ");
+        renderDate.innerHTML = html;
         // console.log(html)
         return html;
     }
@@ -59,17 +88,26 @@ function renderToItems(saveDateArray) {
         return "this ";
     }
 }
+function renderToScrrim() {
+    // saveToLocalStorage(saveDateArray)
+    //  getLocalStorage()
+    // renderToItems(saveDateArray)
+    var data = getLocalStorage();
+    renderDate.innerHTML = renderToItems(data);
+}
 function deleteItemscalculations(uid) {
     try {
         var index = saveDateArray.findIndex(function (items) { return items.uid === uid; });
         if (index === -1)
             throw new Error("we are have Error");
         saveDateArray.splice(index, 1);
-        renderDate.innerHTML = renderToItems(saveDateArray);
+        // add save to local storage function
+        renderToScrrim();
     }
     catch (error) {
         console.log(error);
     }
+    renderToItems(saveDateArray);
 }
 //---controller---//
 //---view---//
@@ -81,3 +119,50 @@ function uid() {
     return "id-" + Math.random();
 }
 //--helper--//
+// let h1=Number(prompt("years"))
+//   let test=h1*12
+//   console.log(test)
+// let action=prompt("choos")
+// function test(h1:number,h2:number,action:string|null){
+//   if(action==="*"){
+//     console.log(h1*h2)
+//   }else if(action==="/"){
+//     console.log(h1/h2)
+//   }else{
+//     console.log("this is no ")
+//   }
+// }
+// test(h1,h2,action)
+function saveToLocalStorage(saveDateArray) {
+    try {
+        // console.log(saveDateArray)
+        // if(!saveDateArray){
+        localStorage.setItem("items", JSON.stringify(saveDateArray));
+        // }else{
+        // console.log("")
+        // throw new Error("thihs is not good");
+        // }
+    }
+    catch (error) {
+        console.log(error);
+    }
+}
+function getLocalStorage() {
+    // console.log(saveDateArray)
+    var data = localStorage.getItem("items");
+    console.log("datasssss", data);
+    if (data == null) {
+        return [];
+    }
+    var parse = JSON.parse(data);
+    console.log("JSON.parse", parse);
+    return parse;
+    // console.log(parse)
+    // console.log("rrrrrrrrrR",d     ate)
+}
+//getLocalStorage()
+// const _items=getLocalStorage()
+// if(_items){
+//   saveDateArray.push(..._items)
+// }
+// renderDate(saveDateArray)
