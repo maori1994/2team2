3a2d28f429daa0b364128370b62888ce0d8c9268
maori1994 <122039133+maori1994@users.noreply.github.com>
2023-05-31 16:53:25 +0300

    MAOR

--- try1/calculation/calculation.ts
+++ try1/calculation/calculation.ts
@@ -2,116 +2,115 @@
 class SendingDataCalculationsClass {
   uid: string;
   constructor(
-    public moneyNeed: number, 
+    public moneyNeed: number,
     public amountOfYears: number,
-    public total:number,){
+    public total: number
+  ) {
     this.uid = uid();
   }
-} 
+}
 let saveDateArray: SendingDataCalculationsClass[] = [];
 //---model---//
 
-let bankYahv=document.querySelector(".bankYahv")as HTMLDivElement;
-let laomi=document.querySelector(".laomi")as HTMLDivElement;
-let discud=document.querySelector(".discud")as HTMLDivElement;
-let egud=document.querySelector(".egud")as HTMLDivElement;
-let international=document.querySelector(".international")as HTMLDivElement;
-let israelPost=document.querySelector(".israelPost")as HTMLDivElement;
-let mercantil=document.querySelector(".mercantile")as HTMLDivElement;
+let bankYahv = document.querySelector(".bankYahv") as HTMLDivElement;
+let laomi = document.querySelector(".laomi") as HTMLDivElement;
+let discud = document.querySelector(".discud") as HTMLDivElement;
+let egud = document.querySelector(".egud") as HTMLDivElement;
+let international = document.querySelector(".international") as HTMLDivElement;
+let israelPost = document.querySelector(".israelPost") as HTMLDivElement;
+let mercantil = document.querySelector(".mercantile") as HTMLDivElement;
 //---controller---//
 function getDateForm(evt) {
   try {
     evt.preventDefault();
     let moneyNeed = evt.target.elements.moneyNeed.value;
     let manyMonths = evt.target.elements.manyMonths.value;
-    let manyMONthsMultiplay=manyMonths*12
-    let total=moneyNeed/manyMONthsMultiplay
-    
+    let manyMONthsMultiplay = manyMonths * 12;
+    let total = moneyNeed / manyMONthsMultiplay;
+
     if (moneyNeed <= 500000) {
-      bankYahv.innerHTML=`<div class="bankYahav">
-        <div><h3>בנק המומלץ</h3></div>
+      bankYahv.innerHTML = `<div class="bankYahav">
+        <div><h3>ההצעה האטרקטיבית ביותר</h3></div>
         <div><img src="../photo/yahav.png" alt="logoOfImg"></div>
         <div class="numberBankYahav">מספר הבנק: 4</div>
-        <div class="contactYahav">*2505 :פלאפון ליצירת קשר </div>
+        <div class="contactYahav">*2505 :מוקד הבנק </div>
         <div class="beforeInterest">סכום התשלום לפני ריבית: ${total}</div>
-        <div class="afterInterest">סכום התשלום כולל ריבית: ${total*1.7}</div>
+        <div class="afterInterest">החזר חודשי כולל ריבית: ${total * 1.7}</div>
         </div>
-      `}
-     else if (moneyNeed <= 750000) {
-      laomi.innerHTML=`<div class="bankLeumi">  
-        <div><h3>בנק המומלץ</h3></div>
+      `;
+    } else if (moneyNeed <= 750000) {
+      laomi.innerHTML = `<div class="bankLeumi">  
+        <div><h3>ההצעה האטרקטיבית ביותר היא של בנק לאומי</h3></div>
         <div><img src="../photo/leumi.png" alt="logoOfImg"></div>
-        <div class="numberbBankLeumi">מספר בנק: 10</div>
-        <div class="contactLeumi">*5522 :פלאפון ליצירת קשר </div>
+        <div class="numberbBankLeumi">מספר הבנק: 10</div>
+        <div class="contactLeumi">*5522 :מוקד הבנק </div>
         <div class="beforeInterestLeumi">סכום התשלום לפני ריבית: ${total}</div>
-        <div class="afterInterestLeumi">סכום התשלום כולל ריבית: ${total*1.6}</div>
+        <div class="afterInterestLeumi">סכום התשלום כולל ריבית: ${
+          total * 1.6
+        }</div>
         </div>
-      `
-     
-    }else if(moneyNeed <=1000000){
-      discud.innerHTML=`<div class="bankEgud>
+      `;
+    } else if (moneyNeed <= 1000000) {
+      discud.innerHTML = `<div class="bankEgud>
         <div><h3>בנק המומלץ</h3></div>
         <div><img src="../photo/diskont.jpg" alt="logoOfImg"></div>
         <div class="numberBankEgud">מספר בנק: 11</div>
         <div class="contactEgud"> *6111 :פלאפון ליצירת קשר</div>
         <div class="beforeInterestEgud">סכום התשלום לפני ריבית: ${total}</div>
-        <div class="afterInterestEgud">סכום התשלום כולל ריבית: ${total*1.5}</div>
+        <div class="afterInterestEgud">סכום התשלום כולל ריבית: ${
+          total * 1.5
+        }</div>
         </div>
-      `
-    } 
-    else if(moneyNeed <=1250000){
-      egud.innerHTML=`
+      `;
+    } else if (moneyNeed <= 1250000) {
+      egud.innerHTML = `
         <div><h3>בנק המומלץ</h3></div>
         <div><img src="../photo/egud.jpg" alt="logoOfImg"></div>
         <div>מספר בנק: 10</div>
         <div>*5522 :פלאפון ליצירת קשר</div>
         <div>סכום התשלום לפני ריבית: ${total}</div>
-        <div>סכום התשלום כולל ריבית: ${total*1.4}</div>
-      `
-    }
-    else if(moneyNeed <=1500000){
-      egud.innerHTML=`
+        <div>סכום התשלום כולל ריבית: ${total * 1.4}</div>
+      `;
+    } else if (moneyNeed <= 1500000) {
+      egud.innerHTML = `
         <div><h3>בנק המומלץ</h3></div>
         <div><img src="../photo/hapoalim.jpg" alt="logoOfImg"></div>
         <div>בנק הפועלים</div>
         <div>מספר בנק: 11</div>
         <div>*2407 :פלאפון ליצירת קשר</div>
         <div>סכום התשלום לפני ריבית: ${total}</div>
-        <div>סכום התשלום כולל ריבית: ${total*1.3}</div>
-      `
-    }
-    
-    else if(moneyNeed <=1750000){
-      israelPost.innerHTML=`
+        <div>סכום התשלום כולל ריבית: ${total * 1.3}</div>
+      `;
+    } else if (moneyNeed <= 1750000) {
+      israelPost.innerHTML = `
         <div><h3>בנק המומלץ </h3></div>
         <div><img src="../photo/israelpost.png" alt="logoOfImg"></div>
         <div>דואר ישראל</div>
         <div>מספר בנק: 12</div>
         <div>*2401 :פלאפון ליצירת קשר</div>
         <div>סכום התשלום לפני ריבית: ${total}</div>
-        <div>סכום התשלום כולל ריבית: ${total*1.2}</div>
-      `
-    }
-   
-    else if(moneyNeed <=2000000){
-      mercantil.innerHTML=`
+        <div>סכום התשלום כולל ריבית: ${total * 1.2}</div>
+      `;
+    } else if (moneyNeed <= 2000000) {
+      mercantil.innerHTML = `
         <div><h3>בנק המומלץ </h3></div>
         <div><img src="../photo/mercantile.jpg" alt="logoOfImg"></div>
         <div>בנק מרכנתיל</div>
         <div>מספר בנק: 13</div>
         <div> *3477 :פלאפון ליצירת קשר</div>
         <div>סכום התשלום לפני ריבית: ${total}</div>
-        <div>סכום התשלום כולל ריבית: ${total*1.1}</div>
-      `
-    }
-    else {
+        <div>סכום התשלום כולל ריבית: ${total * 1.1}</div>
+      `;
+    } else {
       return alert("המספר שרשמת לא תקין");
     }
-   saveDateArray = getLocalStorage()
-    saveDateArray.push(new SendingDataCalculationsClass(moneyNeed,manyMonths,total));
-    saveToLocalStorage(saveDateArray)
-    renderToItems(saveDateArray)
-   } catch (error) {
+    saveDateArray = getLocalStorage();
+    saveDateArray.push(
+      new SendingDataCalculationsClass(moneyNeed, manyMonths, total)
+    );
+    saveToLocalStorage(saveDateArray);
+    renderToItems(saveDateArray);
+  } catch (error) {
     console.log(error);
   }
 }
@@ -131,16 +130,16 @@ function renderToItems(saveDateArray: SendingDataCalculationsClass[]): string {
             `;
       })
       .join(" ");
-    renderDate.innerHTML = html
+    renderDate.innerHTML = html;
     return html;
   } catch (error) {
     console.log(error);
     return `this `;
   }
 }
-function renderToScrrim(){
-  const data = getLocalStorage() 
-  renderDate.innerHTML = renderToItems(data);  
+function renderToScrrim() {
+  const data = getLocalStorage();
+  renderDate.innerHTML = renderToItems(data);
 }
 
 function deleteItemscalculations(uid: string) {
@@ -148,20 +147,19 @@ function deleteItemscalculations(uid: string) {
     let index = saveDateArray.findIndex((items) => items.uid === uid);
     if (index === -1) throw new Error("we are have Error");
     saveDateArray.splice(index, 1);
-    laomi.innerHTML=""
-    bankYahv.innerHTML=""
-    discud.innerHTML=""
-    egud.innerHTML=""
-    international.innerHTML=""
-    israelPost.innerHTML=""
-    mercantil.innerHTML=""
-    renderToScrrim() 
+    laomi.innerHTML = "";
+    bankYahv.innerHTML = "";
+    discud.innerHTML = "";
+    egud.innerHTML = "";
+    international.innerHTML = "";
+    israelPost.innerHTML = "";
+    mercantil.innerHTML = "";
+    renderToScrrim();
   } catch (error) {
     console.log(error);
   }
-  renderToItems(saveDateArray)
-  saveToLocalStorage(saveDateArray)
-  
+  renderToItems(saveDateArray);
+  saveToLocalStorage(saveDateArray);
 }
 //---controller---//
 
@@ -175,67 +173,71 @@ function uid(): string {
 }
 //--helper--//
 
-function saveToLocalStorage(saveDateArray:SendingDataCalculationsClass[]){
+function saveToLocalStorage(saveDateArray: SendingDataCalculationsClass[]) {
   try {
-    localStorage.setItem("items",JSON.stringify(saveDateArray));
+    localStorage.setItem("items", JSON.stringify(saveDateArray));
   } catch (error) {
     console.log(error);
   }
 }
 
-function getLocalStorage():SendingDataCalculationsClass[] {
-  let data : string | null = localStorage.getItem("items")
-  if(data == null) {
-    return []
+function getLocalStorage(): SendingDataCalculationsClass[] {
+  let data: string | null = localStorage.getItem("items");
+  if (data == null) {
+    return [];
   }
-  let parse=JSON.parse(data)
-  console.log("JSON.parse",parse)
+  let parse = JSON.parse(data);
+  console.log("JSON.parse", parse);
   return parse;
-
 }
-
-
-
-
-
-
-
-
 
 // ---- user information--//
 
-class userInformationClass{
-  uid:string
-  constructor(public UserProfile:string,
-      public nameUser:string,
-      public lastName:string,
-      public phoneNumbe:string,
-      public email:string){
-      this.uid=uid()
+class userInformationClass {
+  uid: string;
+  constructor(
+    public UserProfile: string,
+    public nameUser: string,
+    public lastName: string,
+    public phoneNumbe: string,
+    public email: string
+  ) {
+    this.uid = uid();
   }
-} 
+}
 
-let userInformationArray:userInformationClass[]=[]
+let userInformationArray: userInformationClass[] = [];
 
-function getUserInformation(ev){
+function getUserInformation(ev) {
   try {
-      ev.preventDefault()
-      let UserProfile=ev.target.elements.UserProfile.value;
-      let nameUser=ev.target.elements.nameUser.value;
-      let lastName=ev.target.elements.lastName.value;
-      let phoneNumber=ev.target.elements.phoneNumber.value;
-      let email=ev.target.elements.email.value
-      userInformationArray.push(new userInformationClass(UserProfile,nameUser,lastName,phoneNumber,email))
-      renderInformation.innerHTML=renderToUserInformation(userInformationArray)
+    ev.preventDefault();
+    let UserProfile = ev.target.elements.UserProfile.value;
+    let nameUser = ev.target.elements.nameUser.value;
+    let lastName = ev.target.elements.lastName.value;
+    let phoneNumber = ev.target.elements.phoneNumber.value;
+    let email = ev.target.elements.email.value;
+    userInformationArray.push(
+      new userInformationClass(
+        UserProfile,
+        nameUser,
+        lastName,
+        phoneNumber,
+        email
+      )
+    );
+    renderInformation.innerHTML = renderToUserInformation(userInformationArray);
   } catch (error) {
-      console.log(error)
+    console.log(error);
   }
 
-// console.log(UserProfile,nameUser,lastName,phoneNumber,email)
-} 
+  // console.log(UserProfile,nameUser,lastName,phoneNumber,email)
+}
 
-function renderToUserInformation(userInformationArray:userInformationClass[]):string{
-  let htmlOfInformation=userInformationArray.map((userInformation)=>{
+function renderToUserInformation(
+  userInformationArray: userInformationClass[]
+): string {
+  let htmlOfInformation = userInformationArray
+    .map((userInformation) => {
       return `<div>
       <div>${userInformation.UserProfile}</div>
       <div>${userInformation.nameUser}</div>
@@ -244,9 +246,11 @@ function renderToUserInformation(userInformationArray:userInformationClass[]):st
       <div>${userInformation.email}</div>
       </div>
       `;
-
-  }).join(" ")
-  return htmlOfInformation   
+    })
+    .join(" ");
+  return htmlOfInformation;
 }
 
-let renderInformation=document.querySelector(".renderInformation")as HTMLDivElement;
+let renderInformation = document.querySelector(
+  ".renderInformation"
+) as HTMLDivElement;
